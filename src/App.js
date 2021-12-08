7c20a21a6b32fc847cb98832551d3132c69abefd
tgashwinkumar <tgashwinkumar@gmail.com>
2021-12-08 19:45:18 +0530

    Configured Tailwind

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "tailwindcss/tailwind.css";
+import "./styles/tailwind.output.css";
 
 function App() {
   return <div className="w-screen h-screen flex justify-center items-center font-extrabold text-5xl">Hello World</div>;
