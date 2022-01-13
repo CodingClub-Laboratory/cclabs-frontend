d1409c3ef2da20d3982e021bd819c9c178919277
Manuvikash <manuvikashs@gmail.com>
2022-01-13 20:38:02 +0530

    Started collaborators panel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ const Home = () => {
     <React.Fragment>
       <Banner />
       <Terminal />
+      <Collaborator />
     </React.Fragment>
   );
 };
@@ -50,8 +51,8 @@ const Banner = () => {
 
 const Terminal = () => {
   return (
-    <div className="w-screen h-screen md:grid md:grid-cols-2justify-center">
-      <div className="w-3/4 h-2/3 flex-row m-auto md:m-1 text-center md:w-1/4 ">
+    <div className="w-screen h-full md:grid justify-center">
+      <div className="w-3/4 flex-row m-auto md:m-1 text-center md:w-1/4 md:mt-4">
         <h1 className="text-6xl text-blue-700  font-extrabold font-head">
           Create your
         </h1>
@@ -61,21 +62,21 @@ const Terminal = () => {
         <h1 className="text-6xl text-blue-700  font-extrabold font-head">
           with us
         </h1>
-        <p className="py-4">
+        <p className="pt-4">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
           tellus felis, congue at eleifend vel, maximus a arcu. Proin consequat
           nisi quis enim malesuada, ac pretium leo convallis. Donec dictum.
         </p>
       </div>
 
-      <div className=" w-11/12 md:w-1/2 h-1/2 bg-blue-900 rounded-2xl m-auto md:rounded-l-2xl md:absolute md:right-0">
+      <div className=" w-11/12 md:w-1/2 h-1/2 bg-blue-900 rounded-2xl m-auto md:rounded-l-2xl md:absolute md:right-0 mt-5">
         <div className="w-full h-10 bg-blue-600 rounded-t-2xl md:rounded-tl-2xl flex align-center">
           <img src="powershell.png" className="h-7 w-7 mx-3 my-auto" />
           <h1 className="text-2xl text-white font-medium font-body my-auto mx-1">
             Terminal
           </h1>
         </div>
-        <p className="text-white m-3 font-console text-lg">
+        <p className="text-white ml-3 mt-2 font-console text-lg">
           Terminal
           <br /> Copyright (C) CC Labs. All rights reserved.
           <br />
@@ -85,6 +86,24 @@ const Terminal = () => {
           <span className="text-console-green font-semibold"> CCLabs </span>
           <span className="text-console-yellow font-semibold">init</span>
         </p>
+      </div>
+    </div>
+  );
+};
+
+const Collaborator = () => {
+  return (
+    <div className=" w-full h-screen bg-blue-800 mt-32 rounded-xl md:mt-64 m-2 relative">
+      <div className="h-1/4 absolute top-5 right-5 font-head font-extrabold text-4xl md:text-6xl text-right">
+        <span className="text-white">Find your </span>
+        <span className="text-blue-100">
+          <br />
+          dream
+          <br /> collaborators
+        </span>
+        <span className="text-white">
+          <br /> here
+        </span>
       </div>
     </div>
   );
